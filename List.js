0d3fe48e7786ebff8e433e605ef57317cedc3a4b
arvean1.labib@gmail.com <arvean1.labib@gmail.com>
2020-09-28 07:47:19 -0700

    Final Bluetooth

--- List.js
+++ List.js
@@ -21,13 +21,15 @@ export default class List extends Component {
     super();
     this.state = {
       deviceList: {},
+      uuid: null,
+      // deviceList[0] = null Not an Array
     };
     this.listener = props.bleManagerEmitter.addListener(
       'BleManagerDiscoverPeripheral',
       this.handleDevice,
     );
   }
-
+  //Check if there is an event for bluetooth
   async componentDidMount() {
     await BleManager.start();
     this.scan();
@@ -47,14 +49,26 @@ export default class List extends Component {
 
   scan = () => {
     BleManager.scan([], 5);
-    console.log('Scanninig');
+    console.log('Scanning');
+  };
+
+  connect = (uuid) => {
+    BleManager.connect(uuid)
+      .then(() => {
+        console.log('Connected');
+        Alert.alert('Connected! :)');
+      })
+      .catch((error) => {
+        console.log(error);
+        Alert.alert('Failed to connect :(');
+      });
   };
 
   render() {
     const listItems = Object.keys(this.state.deviceList).map((uuid) => (
       <Picker.Item
         key={uuid}
-        label={this.state.deviceList[uuid] || uuid} //Why say OR here?
+        label={this.state.deviceList[uuid] || uuid}
         value={uuid}
       />
     ));
@@ -62,38 +76,12 @@ export default class List extends Component {
       <Picker
         style={{height: 50, width: '100%'}}
         selectedValue={this.state.uuid}
-        onValueChange={
-          (itemValue) => this.setState({uuid: itemValue}) //Do I have to stringify?
-        }>
+        onValueChange={(uuid) => {
+          this.setState({uuid: uuid});
+          this.connect(uuid);
+        }}>
         {listItems}
       </Picker>
     );
   }
 }
-// prop inside > children between
-// unique key ID (key={uuid}), Whenever array of Components
-// names = {"3423jsdf": "Bob", "jwejw2423": "Joe"}
-// names["3423jsdf"]
-// names["jj2u35235"] = "Fred"
-
-// let uuid = blescanresults[i]["uuid"];
-// let name = blescanresults[i]["name"];
-// names = copy(this.state.deviceList);
-// names[uuid] = name
-
-//this.setState({deviceList: names});
-
-// names = {
-//   "3423jsdf": {
-//     "name": "Bob", "other": "blah"
-//   },
-//   ...
-// }
-// names["3423jsdf"]["other"]
-
-//Obejct.keys
-//Key is unique value is not
-//Can do an object in a object
-//Objects store a bunch of key value pairs
-//let copiedPerson = JSON.parse(JSON.stringify(person));
-//Do copy before loop
